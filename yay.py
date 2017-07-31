26e1bef320b86db08c928c376f3276f5a5791b0d
raheeq19-meet <raheeq19@meet.mit.edu>
2017-07-31 18:33:36 +0300

    Stopping here

--- yay.py
+++ yay.py
@@ -20,5 +20,43 @@ def up():
     global direction
     direction = UP
     print("you pressed up!")
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x, y+10)
+    print(turtle.pos())
+def down():
+    global direction
+    direction = DOWN
+    print("you pressed down!")
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x, y-10)
+    print(turtle.pos())
+def left():
+    global direction
+    direction = LEFT
+    print("you pressed left!")
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x-10, y)
+    print(turtle.pos())
+def right():
+    global direction
+    direction = RIGHT
+    print("you pressed right!")
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x+10, y)
+    print(turtle.pos())
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.onkeypress(turtle.stamp, SPACEBAR)
+turtle.listen()
 
-turtle.mainloop()
+
